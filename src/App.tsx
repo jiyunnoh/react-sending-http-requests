11dcc6d0679864e04c9ff7745c5c47087c1847ea
jiyunnoh <ianjy1127@gmail.com>
2022-04-21 17:19:08 -0500

    179. Handling loading & data states

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,22 @@ type FetchedMovieType = {
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
-      const transformedMovies = data.results.map((movieData: FetchedMovieType) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-      setMovies(transformedMovies);
+    const transformedMovies = data.results.map((movieData: FetchedMovieType) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -33,7 +36,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
